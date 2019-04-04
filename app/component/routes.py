7a8d1ba99c54d4dee246987843b0831caf97b930
cutrain <454615501@qq.com>
2019-04-04 19:00:21 +0800

    fix(component): read with 'rb' mode

--- app/component/routes.py
+++ app/component/routes.py
@@ -12,7 +12,7 @@ def compo_list():
     ret = {
         "structure":{},
     }
-    with open(os.path.join('component', 'structure.json'), 'r') as f:
+    with open(os.path.join('component', 'structure.json'), 'rb') as f:
         structure = json.load(f)
     ret['structure'].update(structure)
     return ret
@@ -27,7 +27,7 @@ def compo_params():
     for root, dirs, files in os.walk('component'):
         for file in files:
             if file is not 'structure.json' and file[-5:] == '.json':
-                with open(os.path.join(root, file), 'r') as f:
+                with open(os.path.join(root, file), 'rb') as f:
                     data = json.load(f)
                 if all([field in data for field in fields]):
                     ret['component'].append(data)
