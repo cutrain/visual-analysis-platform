3e2938b77e06802e5cd32169d2a7f75a211a8b51
cutrain <duanyuge@qq.com>
2019-04-27 06:58:41 +0800

    test: initial test script

---
+++ test.py
@@ -0,0 +1,1 @@
+from .test import component, project, data, graph
---
+++ test/graph/basic/graph_instream.py
@@ -0,0 +1,19 @@
+import json
+
+def test():
+    args = {}
+    all_nodes = {[
+        {
+            'node_name':'graph_instream_1',
+            'node_type':'graph_instream',
+            'details':{
+                'path':'graph.json'
+            }
+        },
+        {
+            'node_name':'graph_line_1',
+            'node_type':'graph_line',
+            'details':{
+            }
+        },
+    }
